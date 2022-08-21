248d4f466b6142eef63123f559912c562d316793
Josseline <guardadojosseline@gmail.com>
2022-08-21 00:43:35 -0400

    using axios http client for nodejs

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import EmployeeForm from "./components/EmployeeForm";
 function App() {
   return (
     <div className="App">
- 
-      
+     
       <EmployeeForm/>
      
     </div>
--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,39 +1,117 @@
-import react from "react";
 import "./EmployeeForm.css";
+import { useState } from "react";
+import axios from "axios"; 
 
-function EmployeeForm(){
 
-return(
 
-    <div className="EmployeeForm">
+function EmployeeForm() {
+
+
+    const [userData, setUserData] = useState({
+
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+        userEmail: "",
+    });
+
+
+    const handleChange = (e) => {
+
+        setUserData({ ...userData, [e.target.name]: e.target.value })
+    }
+ 
+
+
+    const handleSubmit = (e) => {
+
+        axios.post('http://localhost:5000/api/test', userData) //requesting data from clients and will post data into database server
+        .then(function (response) {
+            console.log(response);
+           
+          
+        })
+        
+        .catch(function (error) {
+            console.log(error);
+          
+        }); 
+       
+   
+    
+         /*  alert(userData.firstName + " | " + userData.lastName + " | " + userData.phoneNumber + " | " + userData.userEmail); */
+        console.log(userData.firstName + " | " + userData.lastName + " | " + userData.phoneNumber + " | " + userData.userEmail);
+        
+        setUserData({ firstName: "", lastName: "", phoneNumber: "", userEmail: "" })
+        e.preventDefault();
+        
+    
+    }
+   
+   
+ 
+        
+    
+        
+    
+
+    return (
+
+        <div className="EmployeeForm1">
+
             <h1>Hello World!</h1>
 
-        <div className="Container">
+            <div className="Container">
 
-        
-            <div className="form">
-            <form className="formInput">
-                <label>First Name</label>
-                <input type="text">
-                </input>
-                <label>Last Name</label>
-                <input type="text">
-                </input>
-                <label>Phone Number</label>
-                <input type="number">
-                </input>
-                <label>Email</label>
-                <input type="email">
-                </input> 
-                <button type="button">Submit</button>
-            </form>
+                <div className="subContainer">
 
-            </div>        
+                    <form onSubmit={handleSubmit}>
+
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            onChange={handleChange}
+                            value={userData.firstName}>
+                        </input>
+
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            onChange={handleChange}
+                            value={userData.lastName}>
+                        </input>
+
+                        <label>Phone Number</label>
+                        <input
+                            type="number"
+                            name="phoneNumber"
+                            onChange={handleChange}
+                            value={userData.phoneNumber}>
+                        </input>
+
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="userEmail"
+                            onChange={handleChange}
+                            value={userData.userEmail}>
+                        </input>
+
+                        <button
+                            type="submit">
+                            Submit
+                        </button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
         </div>
-
-
-    </div>
-)
+    )
 
 }
 
